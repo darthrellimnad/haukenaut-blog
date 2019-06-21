8c0a2b6c807f251e70f471a2b1480c54f61c8d66
Daniel Miller <dmillerh@gmail.com>
2019-06-21 13:24:14 -0700

    seo title fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Haukenaut Blog Home" />
+    <SEO title="Home" />
     <div
       style={{
         display: "grid",
